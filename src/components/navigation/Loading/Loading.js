b85ad55d0b7601b52d0235e871c64de84435320b
Talison <talisondaniel@hotmail.com>
2019-02-09 12:00:35 -0200

    Loading

---
+++ src/components/navigation/Loading/Loading.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const loading = () => {
+    <div className="cssload-container">
+        <div className="cssload-circle-1">
+            <div className="cssload-circle-2">
+                <div className="cssload-circle-3">
+                    <div className="cssload-circle-4">
+                        <div className="cssload-circle-5">
+                            <div className="cssload-circle-6">
+                                <div className="cssload-circle-7">
+                                    <div className="cssload-circle-8">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+}
